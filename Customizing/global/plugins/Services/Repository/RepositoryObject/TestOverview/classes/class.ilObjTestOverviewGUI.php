999114a3505e8d91d4a872fa8f8c5869bf0e017f
Martin Gorgas <gorgas@hrz.uni-marburg.de>
2024-02-02 22:08:21 +0100

    [umr][plugin][TestOverview] Update Plugin

--- Customizing/global/plugins/Services/Repository/RepositoryObject/TestOverview/classes/class.ilObjTestOverviewGUI.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/TestOverview/classes/class.ilObjTestOverviewGUI.php
@@ -234,8 +234,8 @@ class ilObjTestOverviewGUI extends ilObjectPluginGUI implements ilDesktopItemHan
     public function initCourseTests(): void
     {
         $pnode = $this->tree->getParentNodeData((int) $this->request->getQueryParams()['ref_id']);
-        $otype = ilObject::_lookupType($pnode['ref_id'], true); // Parent node is 'crs'
-        $tsts = $this->tree->getFilteredSubTree($pnode['ref_id'], ['tst']);  // and has 'tst's
+        $otype = ilObject::_lookupType((int) $pnode['ref_id'], true); // Parent node is 'crs'
+        $tsts = $this->tree->getFilteredSubTree((int) $pnode['ref_id'], ['tst']);  // and has 'tst's
 
         $refs = [];
         foreach($tsts as $tst) {
@@ -244,7 +244,7 @@ class ilObjTestOverviewGUI extends ilObjectPluginGUI implements ilDesktopItemHan
 
         $num_nodes = 0;
         foreach($refs as $ref_id) {
-            if($this->access->checkAccess('tst_statistics', '', $ref_id) || $this->access->checkAccess('write', '', $ref_id)) {
+            if($this->access->checkAccess('tst_statistics', '', (int) $ref_id) || $this->access->checkAccess('write', '', (int) $ref_id)) {
                 $this->object->addTest($ref_id);
                 ++$num_nodes;
             }
