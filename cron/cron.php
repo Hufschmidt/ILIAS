81f148264167072d56d48a4dc388e1e1926be108
Thomas Hufschmidt <hufschmidt@hrz.uni-marburg.de>
2024-07-25 16:26:15 +0200

    [ilias][patch] Support to run specific job_id from cli

--- cron/cron.php
+++ cron/cron.php
@@ -8,11 +8,22 @@ chdir('..');
 require_once './Services/Cron/classes/class.ilCronStartUp.php';
 
 if ($_SERVER['argc'] < 4) {
-    echo "Usage: cron.php username password client\n";
+    echo "Usage: cron.php [--jobid <id>] [--username] username [--passsword] password [--client] client\n";
     exit(1);
 }
 
-[$script, $login, $password, $client] = $_SERVER['argv'];
+$rest_index = 1;
+$options = getopt('u:p:c:j:', ['username:', 'password:', 'client:', 'jobid:'], $rest_index);
+
+$login    = array_key_exists('username', $options) ? $options['username'] : $_SERVER['argv'][$rest_index + 0];
+$password = array_key_exists('password', $options) ? $options['password'] : $_SERVER['argv'][$rest_index + 1];
+$client   = array_key_exists('client', $options)   ? $options['client']   : $_SERVER['argv'][$rest_index + 2];
+$job_id   = array_key_exists('jobid', $options)    ? $options['jobid']    : null;
+
+if (!$login || !$password || !$client) {
+    echo "Usage: cron.php [--jobid <id>] [--username] username [--passsword] password [--client] client\n";
+    exit(1);
+}
 
 $cron = new ilCronStartUp(
     $client,
@@ -28,7 +39,13 @@ try {
     $strictCronManager = new ilStrictCliCronManager(
         $DIC->cron()->manager()
     );
-    $strictCronManager->runActiveJobs($DIC->user());
+
+
+    if ($job_id) {
+        $strictCronManager->runJobManual($job_id, $DIC->user());
+    } else {
+        $strictCronManager->runActiveJobs($DIC->user());
+    }
 
     $cron->logout();
 } catch (Exception $e) {
